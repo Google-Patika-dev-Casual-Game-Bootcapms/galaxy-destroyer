2f7342a4d8d4e7ef6dcd30cc78dad7e16bb95711
Halil Coşgun <cosgun.halil@gmail.com>
2021-05-19 20:58:27 +0300

    feat(state): Setup main menu state machine

---
+++ Assets/Game/States/Main/CoPilotSelectionState.cs
@@ -0,0 +1,27 @@
+using Devkit.Base.Component;
+using Devkit.HSM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoPilotSelectionState : StateMachine
+{
+    public CoPilotSelectionState(ComponentContainer componentContainer)
+    {
+    }
+
+    protected override void OnEnter()
+    {
+       
+    }
+
+    protected override void OnExit()
+    {
+        
+    }
+
+    protected override void OnUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Game/States/Main/InventoryState.cs
@@ -0,0 +1,30 @@
+namespace SpaceShooterProject.State 
+{
+    using Devkit.Base.Component;
+    using Devkit.HSM;
+
+    public class InventoryState : StateMachine
+    {
+        public InventoryState(ComponentContainer componentContainer)
+        {
+
+        }
+
+        protected override void OnEnter()
+        {
+            
+        }
+
+        protected override void OnExit()
+        {
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+
+}
+
