2bc55db607f47bc617d10293e4fbee8ba7a5099a
Ege Melis <eayanoglu15@ku.edu.tr>
2021-05-21 21:55:44 +0300

    feat(super-power) add super power information

--- Assets/Game/Components/SuperPowerComponent.cs
+++ Assets/Game/Components/SuperPowerComponent.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SuperPowerComponent : MonoBehaviour
+namespace SpaceShooterProject.Component
 {
-    // Start is called before the first frame update
-    void Start()
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+
+  public class SuperPowerComponent : IComponent
+  {
+    private SuperPowerData superPowerData;
+    private CurrencyComponent currencyComponent;
+
+    public void Initialize(ComponentContainer componentContainer)
     {
-        
+      currencyComponent = componentContainer.GetComponent("CurrencyComponent") as CurrencyComponent;
+
+      Debug.Log("<color=green>Super Power Component initialized!</color>");
+
+      superPowerData = LoadComponent.Load<SuperPowerData>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int GetOwnedLaserItems() {
+      return superPowerData.NumberOfOwnedLaserItems;
     }
+
+    public int GetOwnedShieldItems() {
+      return superPowerData.NumberOfOwnedShieldItems;
+    }
+    
+    public int GetOwnedMegaBombItems() {
+      return superPowerData.NumberOfOwnedMegaBombItems;
+    }
+
+    // Decrease the number of super power items when player use them
+    public void UseLaser() {
+      superPowerData.NumberOfOwnedLaserItems--;
+    }
+
+    public void UseEnergyShield() {
+      superPowerData.NumberOfOwnedEnergyShieldItems--;
+    }
+
+    public void UseMegaBomb() {
+      superPowerData.NumberOfOwnedMegaBombItems--;
+    }
+
+    [Serializable]
+    public struct SuperPowerData
+    {
+      public int NumberOfOwnedLaserItems;
+      public int NumberOfOwnedEnergyShieldItems;
+      public int NumberOfOwnedMegaBombItems;
+    }
+  }
 }
