b08c37c3c34ed8a6dd0eafae605ca8a651c18182
eterzioglu <eterzioglu8@gmail.com>
2021-05-22 19:06:16 +0300

    feat(achievement component) achievement information added

---
+++ Assets/Game/Components/Achievement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooterProject.Component
+{
+    [CreateAssetMenu(menuName = "Achievement")]
+    public class Achievement : ScriptableObject, IObservable<Achievement>
+    {
+        #region Variables
+        [SerializeField] private string achievementName;
+        [SerializeField, TextArea(1, 4)] private string description;
+        [SerializeField] private Sprite icon;
+        [SerializeField] private int goalCount;
+        [SerializeField] private int currentCount;
+        [SerializeField] private bool isAchived;
+        private List<IObserver<Achievement>> observers = new List<IObserver<Achievement>>();
+        #endregion
+
+        #region  Properties
+        public string Name { get => achievementName; }
+        public Sprite Icon { get => icon; }
+        public string Descrption { get => description; }
+        public int GoalCount { get => goalCount; }
+        public int CurrentCount { get => CurrentCount; }
+        #endregion
+
+
+        //raises current count and if currentcount equal or greater than goalcount Notify() observers..
+        public void RaiseCurrentCount()
+        {
+            if (isAchived) return;
+
+            currentCount++;
+
+            if (currentCount >= goalCount) Notify();
+        }
+
+        //notify observers..
+        private void Notify()
+        {
+            foreach (var o in observers)
+                o.OnNext(this);
+        }
+
+        public IDisposable Subscribe(IObserver<Achievement> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Assets/Game/Components/AchievementsComponent.cs
+++ Assets/Game/Components/AchievementsComponent.cs
@@ -1,15 +1,71 @@
 using Devkit.Base.Component;
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
 
-namespace SpaceShooterProject.Component 
+namespace SpaceShooterProject.Component
 {
-    public class AchievementsComponent : IComponent
+    public class AchievementsComponent : IComponent, IObserver<Achievement>
     {
+
+        string path = "Achievements";//we create a folder in Resources folder called Achievements to keep scriptable objects
+        List<Achievement> achievementsList;
+
         public void Initialize(ComponentContainer componentContainer)
         {
             Debug.Log("<color=green>AchievementsComponent initialized!</color>");
+            LoadAchievementsAndAddToList();
         }
 
+        //List the achievements and subscribe them as achievement observer..
+        private void LoadAchievementsAndAddToList()
+        {
+            var achievements = Resources.LoadAll<Achievement>(path);
+
+            for (int i = 0; i < achievements.Length; i++)
+            {
+                achievements[i].Subscribe(this);
+                achievementsList.Add(achievements[i]);
+            }
+        }
+
+        //we raise the achievenemt count which we find with FindAchievementByName function..
+        public void ProgressAchievementWithName(string name)
+        {
+            Achievement achievement = FindAchievementByName(name);
+
+            //if achievement is null, prevent the error
+            if (achievement == null)
+                return;
+
+            achievement.RaiseCurrentCount();
+        }
+
+        //to find the achievement..
+        private Achievement FindAchievementByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return null;
+
+            return achievementsList.Find(a => a.Name.Equals(name));
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Achievement value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
