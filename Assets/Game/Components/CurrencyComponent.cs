0c5adb30f87aa50e8e4ef5c19cbfdad8a07410fc
Berkay KARSLI <berkay.karsli@metu.edu.tr>
2021-05-24 02:45:26 +0300

    feat(CurrencyComponent): Update first code
    -Currency methods have been created
    -Get methods have been created

--- Assets/Game/Components/CurrencyComponent.cs
+++ Assets/Game/Components/CurrencyComponent.cs
@@ -1,18 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CurrencyComponent : MonoBehaviour
+namespace SpaceShooterProject.Component 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Devkit.Base.Component;
 
-    // Update is called once per frame
-    void Update()
+    public class CurrencyComponent : IComponent
     {
-        
+        #region Variables
+            private AccountComponent accountComponent;
+            private int OwnedGold,OwnedDiamond;
+        #endregion
+
+        public void Initialize(ComponentContainer componentContainer)
+        {
+            accountComponent = componentContainer.GetComponent("AccountComponent") as AccountComponent;
+
+            OwnedDiamond = accountComponent.GetOwnedDiamond();
+            OwnedGold = accountComponent.GetOwnedGold();
+        }
+
+        #region Currency Methods
+            void EarnGold(int goldIncome)
+            {
+                OwnedGold += goldIncome;
+            }
+
+            void EarnDiamond(int diamondIncome)
+            {
+                OwnedDiamond += diamondIncome;
+            }
+
+            void SpendGold(int goldOutcome)
+            {
+                if(OwnedGold < goldOutcome)
+                {
+                    //TODO give an error
+                }
+                else
+                {
+                    OwnedGold -= goldOutcome;
+                }
+            }
+
+            void SpendDiamond(int diamondOutcome)
+            {
+                if(OwnedDiamond < diamondOutcome)
+                {
+                    //TODO give an error
+                }
+                else
+                {
+                    OwnedDiamond -= diamondOutcome;
+                }
+            }
+
+
+        #endregion
+    
+        #region Getter Methods
+            int GetOwnedGold()
+            {
+                return OwnedGold;
+            }
+
+            int GetOwnedDiamond()
+            {
+                return OwnedDiamond;
+            }
+        #endregion
     }
 }
