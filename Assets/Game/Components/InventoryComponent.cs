de3970a8fe9363dc80dac8af6a1b3205b85bb698
Oğuzhan Akbulut <oguzhanakbulut@trakya.edu.tr>
2021-05-25 20:23:28 +0300

    feat(inventory): Add Getter Methods
    Getter methods for Gacha System to use & improvement on GachaLoot Method

--- Assets/Game/Components/InventoryComponent.cs
+++ Assets/Game/Components/InventoryComponent.cs
@@ -12,37 +12,38 @@
         public void Initialize(ComponentContainer componentContainer)
         {
             
-            // To-Do: Save & Load Data
+            // To-Do: Save & Load Data?
 
              if (true) // if data not exist
-            {
+             {
                 FirstInitialization();
-            }
+             }
 
             Debug.Log("<color=green>Inventory Component initialized!</color>");
         }
 
         public void FirstInitialization()
         {
+            // Is there any better way?
             inventoryData.SpaceShips = new List<SpaceShipData>()
             {
-                new SpaceShipData { ID = 0, Name = "Gemi 1"},
-                new SpaceShipData { ID = 1, Name = "Gemi 2"},
-                new SpaceShipData { ID = 2, Name = "Gemi 3"}
+                // new SpaceShipData { ID = 0, Name = "Gemi 1"},
+                // new SpaceShipData { ID = 1, Name = "Gemi 2"},
+                // new SpaceShipData { ID = 2, Name = "Gemi 3"}
             };
 
             inventoryData.PermanentCards = new List<CardData>()
             {
-                new CardData { ID = 0, Name = "Kuvvetli Refleksler", Description = "Hareket hızı artar." },
-                new CardData { ID = 1, Name = "Füze Hasarı", Description = "Füze hasarı artar." },
-                new CardData { ID = 2, Name = "Son Şans", Description = "Ölümcül bir hasar aldığındığında bir süreliğine koruyucu bir kalkan devreye girer." },
-                new CardData { ID = 3, Name = "Destek Kuvveti", Description = "Düşman saldırılarını savuşturan bir drone belirir." },
+                // new CardData { ID = 0, Name = "Kuvvetli Refleksler", Description = "Hareket hızı artar." },
+                // new CardData { ID = 1, Name = "Füze Hasarı", Description = "Füze hasarı artar." },
+                // new CardData { ID = 2, Name = "Son Şans", Description = "Ölümcül bir hasar aldığındığında bir süreliğine koruyucu bir kalkan devreye girer." },
+                // new CardData { ID = 3, Name = "Destek Kuvveti", Description = "Düşman saldırılarını savuşturan bir drone belirir." },
             };
 
             inventoryData.TemporalCards = new List<CardData>()
             {
-                new CardData { ID = 0, Name = "Leprikon", Description = "Düşmanlar daha fazla altın düşürürler." },
-                new CardData { ID = 1, Name = "Ekstra Muhimmat", Description = "Bölüme başlarken bir süper güç kullanım hakkı kazanılır." }
+                // new CardData { ID = 0, Name = "Leprikon", Description = "Düşmanlar daha fazla altın düşürürler." },
+                // new CardData { ID = 1, Name = "Ekstra Muhimmat", Description = "Bölüme başlarken bir süper güç kullanım hakkı kazanılır." }
             };
 
             inventoryData.OwnedPermanentCards = new List<int>();
@@ -54,19 +55,19 @@
         // Get return value from Gacha Component and add looted item to inventory
         public void GachaLoot(int chestReturn)
         {
-            if (chestReturn < 4)
+            if (chestReturn < inventoryData.PermanentCards.Count)
             {
                 inventoryData.OwnedPermanentCards.Add(chestReturn);
             }
-            else if (chestReturn > 9 && chestReturn < 12)
+            else if (chestReturn >= 10 && chestReturn < inventoryData.TemporalCards.Count + 10)
             {
                 inventoryData.OwnedTemporalCards.Add(chestReturn - 10);
             }
-            else if (chestReturn > 19 && chestReturn < 32)
+            else if (chestReturn >= 20 && chestReturn < inventoryData.SpaceShips.Count * 4)
             {
                 int index = chestReturn - 20;
                 
-                while (index > 3)
+                while (index > inventoryData.SpaceShips.Count)
                 {
                     index -= 4;
                 }
@@ -90,6 +91,21 @@
             return inventoryData.OwnedSpaceShips;
         }
 
+        public int GetSpaceShipCount()
+        {
+            return inventoryData.SpaceShips.Count;
+        }
+
+        public int GetPermanentCardCount()
+        {
+            return inventoryData.PermanentCards.Count;
+        }
+
+        public int GetTemporalCardCount()
+        {
+            return inventoryData.TemporalCards.Count;
+        }
+
     }
 
     [Serializable]
@@ -104,7 +120,8 @@
         public List<int> OwnedSpaceShips;
         public int[] CollectedSpaceShipParts;
     }
-
+    
+    // To-Do: Store Card & Spaceship buffs?
     [Serializable]
     public struct SpaceShipData
     {
