576e21f3dcfc9f4a200f2904d83664c8af3aafb3
Mertcan Aslan <mertcanaslan198@gmail.com>
2021-05-25 18:22:38 +0300

    feat(copilot): Add Co-Pilot Methods
    -Co-Pilot script has been created
    -First Co-Pilot's method has been written
    -Other Co-Pilot's methods have been created

---
+++ Assets/Game/Components/CoPilotComponent.cs
@@ -0,0 +1,66 @@
+﻿namespace SpaceShooterProject.Component 
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using Devkit.Base.Component;
+	using UnityEngine;
+
+	public class CoPilotComponent : IComponent {
+		GamePlayComponent gamePlayComponent;
+		GameStatesComponent gameStatesComponent;
+
+		public void Initialize(ComponentContainer componentContainer)
+		{
+			gamePlayComponent = componentContainer.GetComponent("GamePlayComponent") as GamePlayComponent;
+			gameStatesComponent = componentContainer.GetComponent("GameStatesComponent") as GameStatesComponent;
+		}
+
+		// public int[] GetCopilotSetting(){
+
+		// }
+
+		// public int WaitfOritPower(){
+		// 	float cooldown = 5.0f;
+		// 	float timer = 0f;
+		// 	int addBulletPower = 1;
+		// 	int maxBulletPower = 10;
+		// 	int currentBulletPower = ship.bulletPower;
+
+		// 	while(true){
+				
+		// 		if(gameStatesComponent.CurrentState !=  gameStatesComponent.Gameplay){
+					
+		// 			break;
+		// 		}
+		// 		else{
+		// 			if(timer >= cooldown ){
+		// 				timer = 0f;
+		// 				currentBulletPower += addBulletPower;
+		// 				if(currentBulletPower>=maxBulletPower){
+		// 					break;
+		// 				}
+		// 			}
+		// 		}
+		// 		yield return currentBulletPower;
+		// 	}
+		// }
+
+		
+		// public void FirethRower(){
+
+		// }
+
+		// public void GotyoUrback(){
+
+		// }
+		
+		// public void NowyoUdont(){
+
+		// }
+		
+		// public void ShocKwave(){
+
+		// }
+
+	}
+}
