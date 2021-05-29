2d7b1c83806460a27d8e0d9517ad1a36ddf74297
Halil Coşgun <cosgun.halil@gmail.com>
2021-05-29 13:57:01 +0300

    feat(ui): Add main menu canvas

---
+++ Assets/Game/UserInterface/MainMenu/MainMenuCanvas.cs
@@ -0,0 +1,12 @@
+namespace SpaceShooterProject.UserInterface 
+{
+    public class MainMenuCanvas : BaseCanvas
+    {
+        protected override void Init()
+        {
+            
+        }
+    }
+}
+
+
