9bc6d84272515e410c47dd9907ca84da8b3ac4ca
Halil Coşgun <cosgun.halil@gmail.com>
2021-05-30 13:09:52 +0300

    feat(ui): Add menu transition requests

--- Assets/Game/UserInterface/MainMenu/MainMenuCanvas.cs
+++ Assets/Game/UserInterface/MainMenu/MainMenuCanvas.cs
@@ -2,9 +2,83 @@ namespace SpaceShooterProject.UserInterface
 {
     public class MainMenuCanvas : BaseCanvas
     {
+        public delegate void MenuRequestDelegate();
+        public event MenuRequestDelegate OnInGameMenuRequest;
+        public event MenuRequestDelegate OnSettingsMenuRequest;
+        public event MenuRequestDelegate OnAchievementsMenuRequest;
+        public event MenuRequestDelegate OnMarketMenuRequest;
+        public event MenuRequestDelegate OnInventoryMenuRequest;
+        public event MenuRequestDelegate OnGarageMenuRequest;
+        public event MenuRequestDelegate OnCoPilotMenuRequest;
+        public event MenuRequestDelegate OnCreditsMenuRequest;
+
         protected override void Init()
         {
             
+        }
+
+        public void RequestInGameMenu() 
+        {
+            if (OnInGameMenuRequest != null) 
+            {
+                OnInGameMenuRequest();
+            }
+        }
+
+        public void RequestSettingsMenu() 
+        {
+            if (OnSettingsMenuRequest != null) 
+            {
+                OnSettingsMenuRequest();
+            }
+        }
+
+        public void RequestAchievementsMenu()
+        {
+            if (OnAchievementsMenuRequest != null)
+            {
+                OnAchievementsMenuRequest();
+            }
+        }
+
+        public void RequestMarketMenu()
+        {
+            if (OnMarketMenuRequest != null)
+            {
+                OnMarketMenuRequest();
+            }
+        }
+
+        public void RequestInventoryMenu()
+        {
+            if (OnInventoryMenuRequest != null)
+            {
+                OnInventoryMenuRequest();
+            }
+        }
+
+        public void RequestGarageMenu()
+        {
+            if (OnGarageMenuRequest != null)
+            {
+                OnGarageMenuRequest();
+            }
+        }
+
+        public void RequestCoPilotMenu()
+        {
+            if (OnCoPilotMenuRequest != null)
+            {
+                OnCoPilotMenuRequest();
+            }
+        }
+
+        public void RequestCreditsMenu()
+        {
+            if (OnCreditsMenuRequest != null)
+            {
+                OnCreditsMenuRequest();
+            }
         }
     }
 }
