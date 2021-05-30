690ddef144502dbd8d30e8fe3bd2bd51abadf8d6
ozgeaydn <ozgeaydin937@gmail.com>
2021-05-31 00:04:04 +0300

    Update AudioComponent.cs

--- Assets/Game/Components/AudioComponent.cs
+++ Assets/Game/Components/AudioComponent.cs
@@ -12,42 +12,21 @@ namespace SpaceShooterProject.Component
             Debug.Log("Audio Component initialized!");
         }
         
-        public static AudioComponent instance;
+        // public static AudioComponent instance;
 
         [SerializeField]
         Sound[] sounds;
 
-        void Awake()
-        {
-            if(instance != null)
-            {
-                 Debug.LogError("More than one AudioManager in the scene.");
-            }
-
-            else
-            {
-                 instance = this;
-             }
-        }
-
-        void Start()
+        public void PlaySound(string _name)
         {
             for(int i=0 ; i<sounds.Length ; i++)
             {
-                GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
-                 _go.transform.SetParent(this.transform);
-                sounds[i].SetSource(_go.AddComponent<AudioSource>());
-            }
-
-        }
-
-        public void playSound(string _name)
-        {
-            for(int i=0 ; i<sounds.Length ; i++)
-            {
+                
                 if(sounds[i].name == _name)
                 {
+                    sounds[i].SetSource(gameObject.AddComponent<AudioSource>());
                     sounds[i].Play();
+                    
                     return;
                 }
             }
@@ -55,6 +34,21 @@ namespace SpaceShooterProject.Component
             //no sound with _name
              Debug.LogWarning("AudioComponent: Sound not found in list" + _name);
 
+        }
+
+        public void PlayMusic(string music)
+        {
+            for(int i=0 ; i<sounds.Length ; i++)
+            {
+                if(sounds[i].name == music)
+                {
+                    sounds[i].SetSource(gameObject.AddComponent<AudioSource>());
+                    sounds[i].Play();
+                    sounds[i].source.loop = true;
+
+                    return;
+                }
+            }
         }
   
     }
@@ -68,7 +62,7 @@ namespace SpaceShooterProject.Component
         [Range(0f , 1f)]
         public float volume = 0.7f;
 
-        private AudioSource source;
+        public AudioSource source;
 
         public void SetSource(AudioSource _source)
         {
@@ -83,5 +77,3 @@ namespace SpaceShooterProject.Component
 
     }
 }
-
-
