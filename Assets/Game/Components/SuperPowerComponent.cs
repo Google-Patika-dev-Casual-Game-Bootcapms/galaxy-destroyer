55dc48f7811176007fac1d661cce5ffe5c2f6f69
Ege Melis <eayanoglu15@ku.edu.tr>
2021-06-01 00:52:01 +0300

    feat(super-power): Update super power compenent
    need to get super-power data from account compenent
    added a suggestion for account-compent about the necessary method

--- Assets/Game/Components/SuperPowerComponent.cs
+++ Assets/Game/Components/SuperPowerComponent.cs
@@ -1,60 +1,85 @@
 namespace SpaceShooterProject.Component
 {
-    using Devkit.Base.Component;
-    using System;
-    using UnityEngine;
+  using Devkit.Base.Component;
+  using System;
+  using UnityEngine;
 
-    public class SuperPowerComponent : IComponent
+  public class SuperPowerComponent : IComponent
+  {
+    public enum SuperPowerType { Laser, Shield, MegaBomb};
+
+    private AccountComponent accountComponent;
+
+    private SuperPowerData laser;
+    private SuperPowerData shield;
+    private SuperPowerData megaBomb;
+
+
+    public void Initialize(ComponentContainer componentContainer)
     {
-        private SuperPowerData superPowerData;
-        private CurrencyComponent currencyComponent;
+      Debug.Log("<color=green>Super Power Component initialized!</color>");
+      accountComponent = componentContainer.GetComponent("AccountComponent") as AccountComponent;
 
-        public void Initialize(ComponentContainer componentContainer)
-        {
-            currencyComponent = componentContainer.GetComponent("CurrencyComponent") as CurrencyComponent;
-
-            Debug.Log("<color=green>Super Power Component initialized!</color>");
-
-
-        }
-
-        public int GetOwnedLaserItems()
-        {
-            return superPowerData.NumberOfOwnedLaserItems;
-        }
-
-        public int GetOwnedShieldItems()
-        {
-            return superPowerData.NumberOfOwnedMegaBombItems;
-        }
-
-        public int GetOwnedMegaBombItems()
-        {
-            return superPowerData.NumberOfOwnedMegaBombItems;
-        }
-
-        // Decrease the number of super power items when player use them
-        public void UseLaser()
-        {
-            superPowerData.NumberOfOwnedLaserItems--;
-        }
-
-        public void UseEnergyShield()
-        {
-            superPowerData.NumberOfOwnedEnergyShieldItems--;
-        }
-
-        public void UseMegaBomb()
-        {
-            superPowerData.NumberOfOwnedMegaBombItems--;
-        }
-
-        [Serializable]
-        public struct SuperPowerData
-        {
-            public int NumberOfOwnedLaserItems;
-            public int NumberOfOwnedEnergyShieldItems;
-            public int NumberOfOwnedMegaBombItems;
-        }
+      // Following methos can be added to Account compenent
+      /*
+      public SuperPowerData[] GetSuperPowerDatas(){
+      return accountData.SuperPowerDatas;
     }
+    */
+
+    // TODO: Initializate super power components
+    // SuperPowerData[] superPowerDatas = accountComponent.GetSuperPowerDatas();
+    // laser = superPowerDatas[0];
+    // shield = superPowerDatas[1];
+    // megaBomb = superPowerDatas[2];
+
+  }
+
+  public void upgradeSuperPower(SuperPowerType type) {
+    switch(type) {
+      case SuperPowerTypes.Laser:
+      laser.Upgrade();
+      break;
+      case SuperPowerTypes.Laser:
+      shield.Upgrade();
+      break;
+      case SuperPowerTypes.MegaBomb:
+      megaBomb.upgrade();
+      break;
+      default:
+      break;
+    }
+  }
+
+  public void GetUpgradePrice(SuperPowerType type) {
+    switch(type) {
+      case SuperPowerTypes.Laser:
+      laser.GetUpgradePrice();
+      break;
+      case SuperPowerTypes.Laser:
+      shield.GetUpgradePrice();
+      break;
+      case SuperPowerTypes.MegaBomb:
+      megaBomb.GetUpgradePrice();
+      break;
+      default:
+      break;
+    }
+  }
+
+  [Serializable]
+  public struct SuperPowerData
+  {
+    public SuperPowerType type { get; };
+    public int level { get; };
+    public List<int> upgradePriceArray { get; };
+
+    public void Upgrade() {
+      level++;
+    }
+
+    public void GetUpgradePrice() {
+      upgradePriceArray[level];
+    }
+  }
 }
