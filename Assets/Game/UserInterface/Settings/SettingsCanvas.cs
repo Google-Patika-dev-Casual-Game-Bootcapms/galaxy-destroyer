01d6edd07d89037c453e76e7296f01a48f34d7b5
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-06-06 02:15:06 +0300

    feat(Settings-Canvas): Create setting-canvas scene

--- Assets/Game/UserInterface/Settings/SettingsCanvas.cs
+++ Assets/Game/UserInterface/Settings/SettingsCanvas.cs
@@ -1,10 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace SpaceShooterProject.UserInterface
 {
     public class SettingsCanvas : BaseCanvas
     {
+        [SerializeField] private Slider volumeSlider;
+
         protected override void Init()
         {
 
+        }
+
+        public float GetVolume()
+        {
+            return volumeSlider.value;
         }
     }
 }
