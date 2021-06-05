9f4ce931fada5ebab312f2eb477ed3b6491e39a3
Ilkin Mammadzada <milkin9798@gmail.com>
2021-06-05 11:04:29 +0400

    feat(Main-Menu-Canvas): main-menu-canvas scene is created, main-menu UI and script are done.

--- Assets/Game/UserInterface/MainMenu/MainMenuCanvas.cs
+++ Assets/Game/UserInterface/MainMenu/MainMenuCanvas.cs
@@ -1,8 +1,16 @@
-namespace SpaceShooterProject.UserInterface 
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
+
+namespace SpaceShooterProject.UserInterface
 {
     public class MainMenuCanvas : BaseCanvas
     {
         public delegate void MenuRequestDelegate();
+
         public event MenuRequestDelegate OnInGameMenuRequest;
         public event MenuRequestDelegate OnSettingsMenuRequest;
         public event MenuRequestDelegate OnAchievementsMenuRequest;
@@ -12,22 +20,101 @@ namespace SpaceShooterProject.UserInterface
         public event MenuRequestDelegate OnCoPilotMenuRequest;
         public event MenuRequestDelegate OnCreditsMenuRequest;
 
+        [SerializeField] private GameObject planets;
+        [SerializeField] private float animationDuration;
+        [SerializeField] private int activePlanetIndex = 0;
+        [SerializeField] private TMP_Text planetName;
+        [SerializeField] private RectTransform backgroundImage;
+
+        private List<RectTransform> planetList;
+        private bool planetMoveCompleted = true;
+
         protected override void Init()
         {
-            
+            backgroundImage.sizeDelta = GetCanvasSize();
+            planetList = planets.GetComponentsInChildren<RectTransform>().ToList();
+            planetName.text = planetList[0].gameObject.name;
         }
 
-        public void RequestInGameMenu() 
+        public void RightButton()
         {
-            if (OnInGameMenuRequest != null) 
+            if (planetMoveCompleted)
+            {
+                planetMoveCompleted = false;
+                for (var i = 0; i < planetList.Count; i++)
+                {
+                    if (i == planetList.Count - 1)
+                        MovePlanet(planetList[i], planetList[0]);
+                    else
+                        MovePlanet(planetList[i], planetList[i + 1]);
+                }
+
+                activePlanetIndex--;
+                if (activePlanetIndex < 0)
+                    activePlanetIndex = planetList.Count - 1;
+                planetName.text = planetList[activePlanetIndex].gameObject.name;
+            }
+        }
+
+        public void LeftButton()
+        {
+            if (planetMoveCompleted)
+            {
+                planetMoveCompleted = false;
+
+                for (var i = 0; i < planetList.Count; i++)
+                {
+                    if (i == 0)
+                        MovePlanet(planetList[i], planetList[planetList.Count - 1]);
+                    else
+                        MovePlanet(planetList[i], planetList[i - 1]);
+                }
+
+                activePlanetIndex++;
+                if (activePlanetIndex > planetList.Count - 1)
+                    activePlanetIndex = 0;
+                planetName.text = planetList[activePlanetIndex].gameObject.name;
+            }
+        }
+
+        public void MovePlanet(RectTransform self, RectTransform target)
+        {
+            self.DOMove(target.position, animationDuration)
+                .SetEase(Ease.InOutSine);
+            self.DOScale(target.localScale, animationDuration)
+                .OnStart(() => { planetMoveCompleted = false; })
+                .SetEase(Ease.InOutSine)
+                .OnComplete(() => { planetMoveCompleted = true; });
+        }
+
+        private Vector2 GetCanvasSize()
+        {
+            Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+            CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
+            var m_ScreenMatchMode = canvasScaler.screenMatchMode;
+            var m_ReferenceResolution = canvasScaler.referenceResolution;
+            var m_MatchWidthOrHeight = canvasScaler.matchWidthOrHeight;
+
+            float scaleFactor = 0;
+            float logWidth = Mathf.Log(screenSize.x / m_ReferenceResolution.x, 2);
+            float logHeight = Mathf.Log(screenSize.y / m_ReferenceResolution.y, 2);
+            float logWeightedAverage = Mathf.Lerp(logWidth, logHeight, m_MatchWidthOrHeight);
+            scaleFactor = Mathf.Pow(2, logWeightedAverage);
+
+            return new Vector2(screenSize.x / scaleFactor, screenSize.y / scaleFactor);
+        }
+
+        public void RequestInGameMenu()
+        {
+            if (OnInGameMenuRequest != null)
             {
                 OnInGameMenuRequest();
             }
         }
 
-        public void RequestSettingsMenu() 
+        public void RequestSettingsMenu()
         {
-            if (OnSettingsMenuRequest != null) 
+            if (OnSettingsMenuRequest != null)
             {
                 OnSettingsMenuRequest();
             }
@@ -80,7 +167,15 @@ namespace SpaceShooterProject.UserInterface
                 OnCreditsMenuRequest();
             }
         }
+
+        public GameObject ActivePlanet()
+        {
+            return planetList[activePlanetIndex].gameObject;
+        }
+
+        public string ActivePlanetName()
+        {
+            return ActivePlanet().name;
+        }
     }
 }
-
-
