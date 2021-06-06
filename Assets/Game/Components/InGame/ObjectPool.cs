20c7dedaeae8bf1529097886790673f3e7d2afa9
Yuceldd <75229158+Yuceldd@users.noreply.github.com>
2021-06-06 17:06:20 +0300

    feat(gameplay): ObjectPool System has been added for fire system.
    ObjectPool System has been added in InGame folder.

---
+++ Assets/Game/Components/InGame/ObjectPool.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool SharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    void Awake()
+    {
+        SharedInstance = this;
+        
+    }
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(objectToPool);
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+        }
+    }
+    public GameObject GetPooledObject()
+     {
+         for(int i = 0; i < amountToPool; i++)
+         {
+             if(!pooledObjects[i].activeInHierarchy)
+             {
+                 return pooledObjects[i];
+             }
+         }
+         return null;
+     }
+  
+    
+}
