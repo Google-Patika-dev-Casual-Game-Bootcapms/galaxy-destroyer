757c257e4c9d5e868016b358b0919bfcd2527603
furkan-bulgur <81180218+furkan-bulgur@users.noreply.github.com>
2021-06-13 18:52:18 +0300

    feat(gameplay): add in game weapon upgrade system
    Co-Authored-By: Murat Han <83009554+maydogan17@users.noreply.github.com>

--- Assets/Game/Components/InGame/GamePlayComponent.cs
+++ Assets/Game/Components/InGame/GamePlayComponent.cs
@@ -36,5 +36,72 @@ namespace SpaceShooterProject.Component
         {
         }
     }
+
+    public class WeaponUpgradeLevelComponent : IComponent //datayı yönetir
+    {
+        private const int maxWeaponLevel = 10;
+        private const float firePowerUpgradeMultiplier = 0.1f; // Ex: 0.1f : level 1: 1f level 2: 1.1f ..... level 10: 1.9f
+        private const float fireRateUpgradeMultiplier = 0.1f;
+        private int level = 1; // Level is between 1 to 10
+
+        private float[] weaponFirePowerData;
+        private float[] weaponFireRateData;
+
+        public WeaponUpgradeLevelComponent()
+        {
+            weaponFirePowerData = new float[maxWeaponLevel];
+            weaponFireRateData = new float[maxWeaponLevel];
+
+            for(int i = 0; i < maxWeaponLevel; i++)
+            {
+                weaponFirePowerData[i] = 1f + (i * firePowerUpgradeMultiplier);
+                weaponFireRateData[i] = 1f + (i * fireRateUpgradeMultiplier);
+            }
+        }
+
+        public void SetLevel(int level)
+        {
+            if(level <= maxWeaponLevel && level >= 1)
+            {
+                this.level = level;
+            }
+            else
+            {
+                Debug.Log("Invalid Weapon Level. ");
+            }
+            
+        }
+
+        public float GetCurrentFirePower()
+        {
+            return weaponFirePowerData[level-1];
+        }
+
+        public float GetCurrentFireRate()
+        {
+            return weaponFireRateData[level-1];
+        }
+
+        public void UpgradeWeaponLevel()
+        {
+            if(level < maxWeaponLevel)
+            {
+                level++;
+            }
+            else
+            {
+                Debug.Log("Max Level Reached. ");
+            }
+
+        }
+
+        public void Initialize(ComponentContainer componentContainer)
+        {
+            componentContainer.AddComponent("WeaponUpgradeLevelComponent", this);
+        }
+    }
+
+
+
 }
 
