71164d57a210dd3f492c8fc7db0c710f3cddc7e6
Halil Coşgun <cosgun.halil@gmail.com>
2021-06-13 22:33:08 +0300

    refactor(component): Seperate weapon upgrade component
    Initialize weapon upgrade component

--- Assets/Game/Components/InGame/GamePlayComponent.cs
+++ Assets/Game/Components/InGame/GamePlayComponent.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Devkit.Base.Object;
 using Devkit.HSM;
 using UnityEngine;
-
+using System;
 
 namespace SpaceShooterProject.Component 
 {
@@ -12,13 +12,22 @@ namespace SpaceShooterProject.Component
     {
         [SerializeField] private Player player;
         private InGameInputSystem inputSystem;
+        private InGameWeaponUpgradeComponent weaponUpgradeComponent;
 
         public void Initialize(ComponentContainer componentContainer)
         {
             Debug.Log("<color=green>GamePlayComponent initialized!</color>");
             inputSystem = componentContainer.GetComponent("InGameInputSystem") as InGameInputSystem;
 
+            InitializeWeaponUpgradeComponent(componentContainer);
+
             player.InjectInpuSystem(inputSystem);
+        }
+
+        private void InitializeWeaponUpgradeComponent(ComponentContainer componentContainer)
+        {
+            weaponUpgradeComponent = new InGameWeaponUpgradeComponent();
+            weaponUpgradeComponent.Initialize(componentContainer);
         }
 
         public void CallUpdate()
@@ -28,80 +37,15 @@ namespace SpaceShooterProject.Component
             player.CallUpdate();
         }
 
-        public void CallFixedUpdate()
+        public void OnEnter()
         {
+            //LOAD Level!
         }
 
-        public void CallLateUpdate()
+        public void OnExit()
         {
-        }
-    }
-
-    public class WeaponUpgradeLevelComponent : IComponent //datayı yönetir
-    {
-        private const int maxWeaponLevel = 10;
-        private const float firePowerUpgradeMultiplier = 0.1f; // Ex: 0.1f : level 1: 1f level 2: 1.1f ..... level 10: 1.9f
-        private const float fireRateUpgradeMultiplier = 0.1f;
-        private int level = 1; // Level is between 1 to 10
-
-        private float[] weaponFirePowerData;
-        private float[] weaponFireRateData;
-
-        public WeaponUpgradeLevelComponent()
-        {
-            weaponFirePowerData = new float[maxWeaponLevel];
-            weaponFireRateData = new float[maxWeaponLevel];
-
-            for(int i = 0; i < maxWeaponLevel; i++)
-            {
-                weaponFirePowerData[i] = 1f + (i * firePowerUpgradeMultiplier);
-                weaponFireRateData[i] = 1f + (i * fireRateUpgradeMultiplier);
-            }
-        }
-
-        public void SetLevel(int level)
-        {
-            if(level <= maxWeaponLevel && level >= 1)
-            {
-                this.level = level;
-            }
-            else
-            {
-                Debug.Log("Invalid Weapon Level. ");
-            }
             
         }
-
-        public float GetCurrentFirePower()
-        {
-            return weaponFirePowerData[level-1];
-        }
-
-        public float GetCurrentFireRate()
-        {
-            return weaponFireRateData[level-1];
-        }
-
-        public void UpgradeWeaponLevel()
-        {
-            if(level < maxWeaponLevel)
-            {
-                level++;
-            }
-            else
-            {
-                Debug.Log("Max Level Reached. ");
-            }
-
-        }
-
-        public void Initialize(ComponentContainer componentContainer)
-        {
-            componentContainer.AddComponent("WeaponUpgradeLevelComponent", this);
-        }
     }
-
-
-
 }
 
