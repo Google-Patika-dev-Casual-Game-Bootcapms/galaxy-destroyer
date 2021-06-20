51793399825a48dc2d254afc3107fae6c055040d
Ilkin Mammadzada <milkin9798@gmail.com>
2021-06-21 00:45:32 +0400

    feature(gameplay):New GameCamera prefab and script are added
    New Camera has LateUpdate method call. This update method helps to avoid camera jitter in gameplay.
    
    Co-Authored-By: Oktay Türkdağlı <61520877+oktayturkdagli@users.noreply.github.com>
    Co-Authored-By: batuu007 <81329050+batuu007@users.noreply.github.com>
    Co-Authored-By: çağdaş bilgin <29821795+cagdasbilgin1@users.noreply.github.com>
    Co-Authored-By: özge salman <49956992+sozgesalman@users.noreply.github.com>

--- Assets/Game/Components/InGame/GameCamera.cs
+++ Assets/Game/Components/InGame/GameCamera.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 
 namespace SpaceShooterProject.Component
 {
-    public class GameCamera : MonoBehaviour, IUpdatable
+    public class GameCamera : MonoBehaviour, ILateUpdatable
     {
         [SerializeField] private Camera mainCamera;
         [SerializeField] private float cameraSpeed;
 
+        private bool isAvailable = false;
 
-        public void CallUpdate()
+
+        public void CallLateUpdate()
         {
             mainCamera.transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime, Space.World);
+        }
+
+        public bool IsAvailable
+        {
+            get => isAvailable;
+            set => isAvailable = value;
         }
     }
 }
