628146d3110e383e965c294d058e3cbddd74ce08
Ilkin Mammadzada <milkin9798@gmail.com>
2021-06-21 00:53:19 +0400

    feature(bullet):Bullet prefab tag and layer is changed. The movement logic is not ready yet.
    Co-Authored-By: Oktay Türkdağlı <61520877+oktayturkdagli@users.noreply.github.com>
    Co-Authored-By: batuu007 <81329050+batuu007@users.noreply.github.com>
    Co-Authored-By: çağdaş bilgin <29821795+cagdasbilgin1@users.noreply.github.com>
    Co-Authored-By: özge salman <49956992+sozgesalman@users.noreply.github.com>

--- Assets/Game/Components/InGame/Bullet.cs
+++ Assets/Game/Components/InGame/Bullet.cs
@@ -1,45 +1,85 @@
+using System;
 using System.Collections;
 using System.Security.Cryptography;
+using Devkit.Base.Object;
+using Devkit.Base.Pattern.ObjectPool;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Bullet : MonoBehaviour
+public class Bullet : MonoBehaviour, IPoolable, IUpdatable
 {
-    [SerializeField] 
-    private float speed = 3; 
+    [SerializeField] private float speed = 3;
+    [SerializeField] private RectTransform _transform;
 
-    private Transform myTransform;
+    public delegate void BulletDelegate();
+
+    public BulletDelegate OnHitEnemy;
+
     private float maxVerticalPosition;
     private Vector2 endPosition;
+    float elapsedTime = 0;
+    Vector2 startingPos;
+    private Tween moveTween;
 
- 
+
     private void OnEnable()
     {
-        myTransform = transform;
-
+        startingPos = _transform.position;
         maxVerticalPosition = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, 1)).y;
-        endPosition = new Vector2(myTransform.position.x, maxVerticalPosition + 1);
+        endPosition = new Vector2(startingPos.x, maxVerticalPosition + 1);
 
-        StartCoroutine(Move(speed));
+        //StartCoroutine(Move(speed));
+        moveTween = _transform.DOMoveY(endPosition.y, 2f)
+            .OnComplete(() =>
+            {
+                
+            });
     }
 
-    private void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-        if (myTransform.position.y > maxVerticalPosition)
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            gameObject.SetActive(false);
+            moveTween.Kill();
+            OnHitEnemy();
         }
     }
- 
+
+    private void OnDisable()
+    {
+        moveTween.Kill();
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
     IEnumerator Move(float duration)
     {
-        float elapsedTime = 0;
-        Vector2 startingPos = myTransform.position;
-
         while (elapsedTime < duration)
         {
-            myTransform.position = Vector3.Lerp(startingPos, endPosition, (elapsedTime / duration));
             elapsedTime += Time.unscaledDeltaTime;
             yield return null;
         }
-    }     
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Initialize()
+    {
+    }
+
+    public void CallUpdate()
+    {
+        transform.position = Vector3.Lerp(startingPos, endPosition, speed * Time.deltaTime);
+    }
 }
