e783cf62970e848cc1d26acdd736d531e7284644
rabiatezcan <rabiatezcan@hotmail.com>
2021-06-24 23:09:00 +0300

    fix(ai) : added scene a player.

---
+++ Assets/Game/Enemies/FollowTarget.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : IMovement
+{
+    [SerializeField] private Transform targetObject; 
+    public void Move(Minion minion)
+    {
+        
+    }
+}
