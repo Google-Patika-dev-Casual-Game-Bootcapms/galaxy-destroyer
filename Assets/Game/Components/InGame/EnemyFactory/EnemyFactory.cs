4a5a0000724679ba88f86cdf3b19fb2bcd44fb54
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2021-06-27 16:01:07 +0300

    feat(enemy): Add Enemy Factory
    Co-Authored-By: Ilkin Mammadzada <32387195+ilkinmammadzada220@users.noreply.github.com>
    Co-Authored-By: özge salman <49956992+sozgesalman@users.noreply.github.com>
    Co-Authored-By: batuu007 <81329050+batuu007@users.noreply.github.com>
    Co-Authored-By: çağdaş bilgin <29821795+cagdasbilgin1@users.noreply.github.com>
    Co-Authored-By: Hasan Furkan Balkaç <balkac15@itu.edu.tr>

--- Assets/Game/Components/InGame/EnemyFactory/EnemyFactory.cs
+++ Assets/Game/Components/InGame/EnemyFactory/EnemyFactory.cs
@@ -7,18 +7,43 @@
 
     public class EnemyFactory : IEnemyFactory, IInitializable
     {
+
         private Pool<RoadTracker> roadTrackerPool;
-        private const string SOURCE_OBJECT_PATH = "Prefabs/RoadTrackerEnemy";
+        private const string ROADTRACKER_OBJECT_PATH = "Prefabs/RoadTrackerEnemy";
+
+        private Pool<Kamikaze> kamikazePool;
+        private const string KAMIKAZE_OBJECT_PATH = "Prefabs/KamikazeEnemy";
+
+        private Pool<FlameThrower> FlameThrowerPool;
+        private const string FLAMETHROWER_OBJECT_PATH = "Prefabs/FlameThrowerEnemy";
+
+        private Pool<HeliA14> HeliA14Pool;
+        private const string HELIA14_OBJECT_PATH = "Prefabs/HeliA14Enemy";
+
+        private Pool<HeliA17> HeliA17Pool;
+        private const string HELIA17_OBJECT_PATH = "Prefabs/HeliA17Enemy";
 
         public void Init()
         {
-            roadTrackerPool = new Pool<RoadTracker>(SOURCE_OBJECT_PATH);
+            roadTrackerPool = new Pool<RoadTracker>(ROADTRACKER_OBJECT_PATH);
             roadTrackerPool.PopulatePool(5);
+
+            kamikazePool = new Pool<Kamikaze>(KAMIKAZE_OBJECT_PATH);
+            kamikazePool.PopulatePool(5);
+
+            FlameThrowerPool = new Pool<FlameThrower>(FLAMETHROWER_OBJECT_PATH);
+            FlameThrowerPool.PopulatePool(5);
+
+            HeliA14Pool = new Pool<HeliA14>(HELIA14_OBJECT_PATH);
+            HeliA14Pool.PopulatePool(5);
+
+            HeliA17Pool = new Pool<HeliA17>(HELIA17_OBJECT_PATH);
+            HeliA17Pool.PopulatePool(5);
         }
 
         public void PreInit()
         {
-           // TODO: Refector
+           // TODO: Refactor
         }
 
         public IEnemy ProduceEnemy(EnemyType type)
@@ -28,6 +53,18 @@
             {
                 case EnemyType.RoadTracker:
                     enemy = roadTrackerPool.GetObjectFromPool();
+                    break;
+                case EnemyType.Kamikaze:
+                    enemy = kamikazePool.GetObjectFromPool();
+                    break;
+                case EnemyType.FlameThrower:
+                    enemy = FlameThrowerPool.GetObjectFromPool();
+                    break;
+                case EnemyType.HeliA14:
+                    enemy = HeliA14Pool.GetObjectFromPool();
+                    break;
+                case EnemyType.HeliA17:
+                    enemy = HeliA17Pool.GetObjectFromPool();
                     break;
             }
             return enemy;
