06a1e93830daef8553b3a1cd5e36a2896ae2ab6c
Ilkin Mammadzada <milkin9798@gmail.com>
2021-06-28 00:56:02 +0400

    refactor(gameplay): Added HP to Enemy. Trigger Logic is created
    Co-Authored-By: Oktay Türkdağlı <61520877+oktayturkdagli@users.noreply.github.com>
    Co-Authored-By: eterzioglu <eterzioglu8@gmail.com>
    Co-Authored-By: çağdaş bilgin <29821795+cagdasbilgin1@users.noreply.github.com>
    Co-Authored-By: batuu007 <81329050+batuu007@users.noreply.github.com>
    Co-Authored-By: özge salman <49956992+sozgesalman@users.noreply.github.com>
    Co-Authored-By: Mertcan Aslan <mertcanaslan198@gmail.com>

--- Assets/Game/Components/InGame/Enemy.cs
+++ Assets/Game/Components/InGame/Enemy.cs
@@ -1,12 +1,14 @@
-﻿namespace SpaceShooterProject.Component
-{ 
+﻿using System;
 
+namespace SpaceShooterProject.Component
+{
     using UnityEngine;
     using System.Collections;
 
     public abstract class Enemy : MonoBehaviour, IEnemy
     {
         [SerializeField] private float shipSpeed = 20f;
+        [SerializeField] private int hp = 100;
         [SerializeField] private SpriteRenderer shipSpriteRender;
         [SerializeField] private float frameRate = 0;
         [SerializeField] private float fireRate = 20;
@@ -20,15 +22,33 @@
 
         public abstract void Patrol();
 
-        public abstract void GetHit();
+        public abstract void GetHit(int damage);
 
         public abstract void Death();
 
         public abstract void OutOfScreen();
 
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.gameObject.CompareTag("FriendlyBullet"))
+            {
+                var bullet = collider.gameObject.GetComponent<Bullet>();
+                GetHit(bullet.GetDamage());
+            }
+        }
+
+        public int HP
+        {
+            get => hp;
+            set => hp = value;
+        }
+
         public void CallUpdate()
         {
-            
+            if (HP<=0)
+            {
+                Destroy(gameObject);
+            }
         }
 
         public void Deactivate()
@@ -38,30 +58,22 @@
 
         public void Init()
         {
-            
         }
 
         public void Initialize()
         {
-            
         }
 
         public void InjectBulletCollector(BulletCollector bulletCollector)
         {
-            
         }
 
         public void OnDestruct()
         {
-            
         }
 
         public void PreInit()
         {
-            
         }
-
     }
-
-
 }
