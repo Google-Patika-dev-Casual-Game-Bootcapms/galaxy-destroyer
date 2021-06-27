18bf7233a663438c8c7476884540cd48e3ba741d
Halil Coşgun <cosgun.halil@gmail.com>
2021-06-27 14:39:11 +0300

    refactor(code): Remove regions

--- Assets/Game/Components/Achievement.cs
+++ Assets/Game/Components/Achievement.cs
@@ -7,7 +7,6 @@ namespace SpaceShooterProject.Component
     [CreateAssetMenu(menuName = "Achievement")]
     public class Achievement : ScriptableObject, IObservable<Achievement>
     {
-        #region Variables
         [SerializeField] private int id;
         [SerializeField] private string achievementName;
         [SerializeField, TextArea(1, 4)] private string description;
@@ -18,9 +17,7 @@ namespace SpaceShooterProject.Component
         [SerializeField] private int prize;
         [SerializeField] private bool isAchived;
         private List<IObserver<Achievement>> observers = new List<IObserver<Achievement>>();
-        #endregion
 
-        #region  Properties
         public int Id { get => id; }
         public string Name { get => achievementName; }
         public Sprite Icon { get => icon; }
@@ -30,7 +27,6 @@ namespace SpaceShooterProject.Component
         public int CurrentCount { get => currentCount; }
         public int Prize { get => prize; }
         public bool IsAchived { get => isAchived; set => isAchived = value; }
-        #endregion
 
         //raises current count and if currentcount equal or greater than goalcount Notify() observers..
         public void RaiseCurrentCount()
