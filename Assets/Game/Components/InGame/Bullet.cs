78b41d7cee7f67802494b4cf513793bb14b1e984
Ilkin Mammadzada <milkin9798@gmail.com>
2021-06-28 00:56:22 +0400

    refactor(gameplay): Code is refactored
    Co-Authored-By: Oktay Türkdağlı <61520877+oktayturkdagli@users.noreply.github.com>
    Co-Authored-By: eterzioglu <eterzioglu8@gmail.com>
    Co-Authored-By: çağdaş bilgin <29821795+cagdasbilgin1@users.noreply.github.com>
    Co-Authored-By: batuu007 <81329050+batuu007@users.noreply.github.com>
    Co-Authored-By: özge salman <49956992+sozgesalman@users.noreply.github.com>
    Co-Authored-By: Mertcan Aslan <mertcanaslan198@gmail.com>

--- Assets/Game/Components/InGame/Bullet.cs
+++ Assets/Game/Components/InGame/Bullet.cs
@@ -1,69 +1,72 @@
-using System;
-using System.Collections;
-using System.Security.Cryptography;
-using Devkit.Base.Object;
-using Devkit.Base.Pattern.ObjectPool;
-using DG.Tweening;
-using SpaceShooterProject.Component;
-using UnityEngine;
-using Random = UnityEngine.Random;
-
-public class Bullet : MonoBehaviour, IPoolable
+namespace SpaceShooterProject.Component
 {
-    [SerializeField] private float speed = 3f;
-    [SerializeField] private RectTransform _transform;
+    using Devkit.Base.Object;
+    using Devkit.Base.Pattern.ObjectPool;
+    using UnityEngine;
+    using Random = UnityEngine.Random;
 
-    public delegate void BulletTriggerDelegate();
-
-    public delegate void BulletPoolDelegate(Bullet bullet);
-
-    public BulletTriggerDelegate OnHitEnemy;
-    public BulletPoolDelegate OnBulletOutOfScreen;
-
-    private IBulletCollector bulletCollectorReference;
-    private GameCamera gameCamera;
-
-    private void Update()
+    public class Bullet : MonoBehaviour, IPoolable, IUpdatable
     {
-        _transform.Translate(Vector3.up * (speed + gameCamera.CameraSpeed) * Time.deltaTime,
-            Space.World);
-        if (_transform.position.y > Camera.main.ViewportToWorldPoint(new Vector2(Random.value, 1)).y) 
-        {
-            bulletCollectorReference.AddBulletToPool(this);
-        }
-           
-    }
+        [SerializeField] private float speed = 3f;
+        [SerializeField] private int damage = 10;
+        [SerializeField] private RectTransform _transform;
 
-    public void InjectBulletCollector(BulletCollector bulletCollector) 
-    {
-        if (bulletCollectorReference != null) 
+        public delegate void BulletTriggerDelegate();
+
+        public delegate void BulletPoolDelegate(Bullet bullet);
+
+        public BulletTriggerDelegate OnHitEnemy;
+        public BulletPoolDelegate OnBulletOutOfScreen;
+
+        private IBulletCollector bulletCollectorReference;
+        private GameCamera gameCamera;
+
+        public void Initialize()
         {
-            return;
+            gameCamera = Camera.main.GetComponent<GameCamera>();
         }
 
-        bulletCollectorReference = bulletCollector;
-    }
-
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
+        public void CallUpdate()
         {
-            OnHitEnemy();
+            _transform.Translate(Vector3.up * (speed + gameCamera.CameraSpeed) * Time.deltaTime,
+                Space.World);
+            if (_transform.position.y > Camera.main.ViewportToWorldPoint(new Vector2(Random.value, 1)).y)
+            {
+                bulletCollectorReference.AddBulletToPool(this);
+            }
         }
-    }
 
-    public void Activate()
-    {
-        gameObject.SetActive(true);
-    }
+        public void InjectBulletCollector(BulletCollector bulletCollector)
+        {
+            if (bulletCollectorReference != null)
+            {
+                return;
+            }
 
-    public void Deactivate()
-    {
-        gameObject.SetActive(false);
-    }
+            bulletCollectorReference = bulletCollector;
+        }
 
-    public void Initialize()
-    {
-        gameCamera = Camera.main.GetComponent<GameCamera>();
+        /*private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                OnHitEnemy();
+            }
+        }*/
+
+        public void Activate()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public int GetDamage()
+        {
+            return damage;
+        }
     }
 }
