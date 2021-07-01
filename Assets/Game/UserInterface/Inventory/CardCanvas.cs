653fb6f1a081110bf5ec0487d07c3e227e5a6999
Halil Coşgun <cosgun.halil@gmail.com>
2021-07-01 21:59:20 +0300

    refactor(method): Remove unnecessary method

--- Assets/Game/UserInterface/Inventory/CardCanvas.cs
+++ Assets/Game/UserInterface/Inventory/CardCanvas.cs
@@ -34,23 +34,5 @@ namespace SpaceShooterProject.UserInterface
                 OnReturnToInventory();
             }
         }
-
-
-        private Vector2 GetCanvasSize()
-        {
-            Vector2 screenSize = new Vector2(Screen.width, Screen.height);
-            CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
-            var m_ScreenMatchMode = canvasScaler.screenMatchMode;
-            var m_ReferenceResolution = canvasScaler.referenceResolution;
-            var m_MatchWidthOrHeight = canvasScaler.matchWidthOrHeight;
-
-            float scaleFactor = 0;
-            float logWidth = Mathf.Log(screenSize.x / m_ReferenceResolution.x, 2);
-            float logHeight = Mathf.Log(screenSize.y / m_ReferenceResolution.y, 2);
-            float logWeightedAverage = Mathf.Lerp(logWidth, logHeight, m_MatchWidthOrHeight);
-            scaleFactor = Mathf.Pow(2, logWeightedAverage);
-
-            return new Vector2(screenSize.x / scaleFactor, screenSize.y / scaleFactor);
-        }
     }
 }
