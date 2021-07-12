71b773cd2479e26e83fb0137b7c15ffe49690e96
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-07-12 21:36:46 +0300

    fix(inventory-canvas): Check if Inventory Data Exist

--- Assets/Game/Components/InventoryComponent.cs
+++ Assets/Game/Components/InventoryComponent.cs
@@ -11,25 +11,33 @@
         private InventoryData inventoryData;
 
         private AccountComponent accountComponent;
+        private CardComponent cardComponent;
+
+        private int permanentCardCount;
+        private int temporalCardCount;
 
         public void Initialize(ComponentContainer componentContainer)
         {
             accountComponent = componentContainer.GetComponent("AccountComponent") as AccountComponent;
+            cardComponent = componentContainer.GetComponent("CardComponent") as CardComponent;
+
+            permanentCardCount = cardComponent.GetPermanentCardCount();
+            temporalCardCount = cardComponent.GetTemporalCardCount();
 
 
-            if (accountComponent.IsFileExist())
+            if (!accountComponent.IsInitializedForFirstTime())
             {
                 inventoryData.OwnedPermanentCards = accountComponent.OwnedPermanentCards();
                 inventoryData.OwnedTemporalCards = accountComponent.OwnedTemporalCards();
                 inventoryData.OwnedSpaceShips = accountComponent.GetOwnedSpaceShips();
                 inventoryData.CollectedSpaceShipParts = accountComponent.CollectedSpaceShipParts();
-
-                AddCardsToInventory();
             }
             else
             {
                 FirstInitialization();
             }
+
+            AddCardsToInventory();
 
             Debug.Log("<color=green>Inventory Component initialized!</color>");
         }
@@ -40,20 +48,31 @@
             inventoryData.OwnedTemporalCards = new List<int>();
             inventoryData.OwnedSpaceShips = new List<int>();
             inventoryData.CollectedSpaceShipParts = new int[4];
-
-            AddCardsToInventory();
         }
 
-        // Add cards in order to show in inventory canvas
+        // Add missing cards in order to show in inventory canvas
         private void AddCardsToInventory()
         {
-            AddPermanentCard(0);
-            AddPermanentCard(1);
-            AddPermanentCard(2);
-            AddPermanentCard(3);
+            int upperLimit = Math.Max(temporalCardCount, permanentCardCount);
 
-            AddTemporalCard(0);
-            AddTemporalCard(1);
+            for (int i = 0; i < upperLimit; i++)
+            {
+                if (i < permanentCardCount)
+                {
+                    if (!inventoryData.OwnedPermanentCards.Contains(i))
+                    {
+                        inventoryData.OwnedPermanentCards.Add(i);
+                    }
+                }
+
+                if (i < temporalCardCount)
+                {
+                    if (!inventoryData.OwnedTemporalCards.Contains(i))
+                    {
+                        inventoryData.OwnedTemporalCards.Add(i);
+                    }
+                }
+            }
         }
 
         #region Setter Methods
