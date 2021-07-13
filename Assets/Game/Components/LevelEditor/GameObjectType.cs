49efa0cb4a5b21c3a9ba920c0003f747f55b4b32
serhattatar <mserhattatar@gmail.com>
2021-07-13 19:01:09 +0300

    fix(LevelEditor/GameObjectType) added bool isactive for LevelObjectParallaxManager Component

--- Assets/Game/Components/LevelEditor/GameObjectType.cs
+++ Assets/Game/Components/LevelEditor/GameObjectType.cs
@@ -1,9 +1,11 @@
 /*IMPORTANT!
  * if you want to save GameObject in any level, add this script in your gameobject
  */
+
 using UnityEngine;
 
 public class GameObjectType : MonoBehaviour
 {
     public EGameObjectType Type;
+    public bool isActive;
 }
